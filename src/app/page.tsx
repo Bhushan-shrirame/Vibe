cafd7dc8c4ac4818d8a4b0ee676e3a0c525b3a4f
Bhushan Shrirame <bhushanshrirame24@gmail.com>
2025-09-30 23:27:31 +0530

    trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import { caller, getQueryClient,trpc } from "@/trpc/server";
+import { HydrationBoundary, useQuery } from "@tanstack/react-query";
+import { Client } from "./Client";
+import { Suspense } from "react";
 
 const Page = async () => {
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"bhushan prefetch"}))
+
 
   return(
-    <div>
-      hello world
-    </div>
+    <HydrationBoundary>
+      <Suspense fallback= {<p>Loading...</p>}>
+        <Client/>
+      </Suspense>
+    </HydrationBoundary>
   )
 };
 
